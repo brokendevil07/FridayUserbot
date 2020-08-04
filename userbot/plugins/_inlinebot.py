ea16f6211f8934793651acd5278281c30430621b
brokendevil07 <67486105+brokendevil07@users.noreply.github.com>
2020-08-04 12:51:40 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -93,14 +93,14 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
 
 def paginate_help(page_number, loaded_plugins, prefix):
     number_of_rows = 10
-    number_of_cols = 2
+    number_of_cols = 3
     helpable_plugins = []
     for p in loaded_plugins:
         if not p.startswith("_"):
             helpable_plugins.append(p)
     helpable_plugins = sorted(helpable_plugins)
     modules = [custom.Button.inline(
-        "{} {} {}".format ("✨", x , "✨"),
+        "{} {} {}".format ("💜👿", x , "👿💜"),
         data="us_plugin_{}".format(x))
         for x in helpable_plugins]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
@@ -111,7 +111,7 @@ def paginate_help(page_number, loaded_plugins, prefix):
     if len(pairs) > number_of_rows:
         pairs = pairs[modulo_page * number_of_rows:number_of_rows * (modulo_page + 1)] + \
             [
-            (custom.Button.inline("⏪", data="{}_prev({})".format(prefix, modulo_page)),
-             custom.Button.inline("⏩", data="{}_next({})".format(prefix, modulo_page)))
+            (custom.Button.inline("👈👈", data="{}_prev({})".format(prefix, modulo_page)),
+             custom.Button.inline("👉👉", data="{}_next({})".format(prefix, modulo_page)))
         ]
     return pairs
