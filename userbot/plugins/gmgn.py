34edc218bfaec79a460eb2fe3576068aedc1da07
brokendevil07 <67486105+brokendevil07@users.noreply.github.com>
2020-09-15 12:03:43 +0530

    Add files via upload

---
+++ userbot/plugins/gmgn.py
@@ -0,0 +1,123 @@
+#catuser bot fayash
+
+import asyncio
+from telethon import events
+from telethon.tl import functions, types
+from telethon.tl.types import ChannelParticipantsAdmins
+from platform import uname
+from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd
+
+n = str(ALIVE_NAME) if ALIVE_NAME else "cat"    
+    
+@borg.on(admin_cmd(pattern=r"g1m"))
+async def catg1m(g1m):
+    await g1m.edit("╔═══ ✨🧚🏻✿💫🧚🏻 ══╗\n"
+"ㅤㅤㅤG๏๏๔ ๓๏гภเภG\n"
+"╚═══ 💫🧚🏻✿✨🧚🏻 ══╝\n")
+    
+@borg.on(admin_cmd(pattern=r"g1n"))
+async def catg1n(g1n):
+    await g1n.edit("╔═══ ✨🧚🏻✿💫🧚🏻 ══╗\n"
+"ㅤㅤㅤG๏๏๔ ภเGђt\n"
+"╚═══ 💫🧚🏻✿✨🧚🏻 ══╝\n")
+    
+@borg.on(admin_cmd(pattern=r"g2m"))
+async def catg2m(g2m):
+    await g2m.edit("══❥━━═❥═━━❥════❥\n"
+"G♥️o♥️o♥️d♥️\n"
+"M♥️o♥️r♥️n♥️i♥️n♥️g♥️\n"
+"══❥━━═❥═━━❥════❥\n")
+    
+@borg.on(admin_cmd(pattern=r"g2n"))
+async def catg2n(g2n):
+    await g2n.edit("══❥━━═❥═━━❥════❥\n"
+"G♥️o♥️o♥️d♥️\n"
+"N♥i♥g♥h♥t♥\n"
+"══❥━━═❥═━━❥════❥\n")
+    
+@borg.on(admin_cmd(pattern=r"g3m"))
+async def catg3m(g3m):
+    await g3m.edit("▭▬▭▬▭▬▭▬▭▬\n"
+"◄█𝙂𝙤𝙤𝙙 𝙈𝙤𝙧𝙣𝙞𝙣𝙜█►\n"
+"▭▬▭▬▭▬▭▬▭▬\n")
+    
+@borg.on(admin_cmd(pattern=r"g3n"))
+async def catg3n(g3n):
+    await g3n.edit("▭▬▭▬▭▬▭▬▭▬\n"
+"◄█𝙂𝙤𝙤𝙙 𝙉𝙞𝙜𝙝𝙩█►\n"
+"▭▬▭▬▭▬▭▬▭▬\n")  
+    
+@borg.on(admin_cmd(pattern=r"g4m"))
+async def catg4m(g4m):
+    await g4m.edit("█▀▀▀▀▀▀▀▀▀▀▀█\n"
+"❣️❥V3rY❥900D❥M0rn!n9❣️\n"
+"█▄▄▄▄▄▄▄▄▄▄▄█\n")
+    
+@borg.on(admin_cmd(pattern=r"g4n"))
+async def catg4n(g4n):
+    await g4n.edit("█▀▀▀▀▀▀▀▀▀▀▀█\n"
+"❣️900D❥N!9h7❥5!33pw3!❣️\n"
+"█▄▄▄▄▄▄▄▄▄▄▄█\n") 
+    
+@borg.on(admin_cmd(pattern=r"g5m"))
+async def catg5m(g5m):
+    await g5m.edit("┄┄┉┉»✽̶🌹✿🌹»✽̶┉┉┄┄\n"
+"G🌷o🍃o🌷D\n"
+"M🍃o🌷r🍃n🌷i🍃n🌷G\n"
+"┄┄┉┉»✽̶🌹✿🌹»✽̶┉┉┄┄\n") 
+    
+@borg.on(admin_cmd(pattern=r"g5n"))
+async def catg5n(g5n):
+    await g5n.edit("┄┄┉┉»✽̶🌹✿🌹»✽̶┉┉┄┄\n"
+"G🌷o🍃o🌷D\n"
+"N🍃i🌷G🍃h🌷T\n"
+"┄┄┉┉»✽̶🌹✿🌹»✽̶┉┉┄┄\n")   
+    
+@borg.on(admin_cmd(pattern=r"g6m"))
+async def catg6m(g6m):
+    await g6m.edit("。.・゜✭・.・✫・・゜✭・..・✫\n"
+"⌯ 🕷•°°•ğ¤¤ď ɱ¤ȑñȋñğ•°°•🕷⌯ .\n"
+"。.・゜✭・.・✫・・゜✭・..・✫\n")
+    
+@borg.on(admin_cmd(pattern=r"g6n"))
+async def catg6n(g6n):
+    await g6n.edit("。.・゜✭・.・✫・・゜✭・..・✫\n"
+"⌯ 🕷•°°• ğ¤¤ď ñȋğȟț •°°•🕷⌯ .\n"
+"。.・゜✭・.・✫・・゜✭・..・✫\n")
+    
+@borg.on(admin_cmd(pattern=r"g7m"))
+async def catg7m(g7m):
+    await g7m.edit("┌──❀*̥˚──◌──❀*̥˚─┐\n"
+"   𖤐 𝘎𝘔....𝘏𝘢𝘷𝘦 𝘢 𝘨𝘙8 𝘥𝘈𝘺 𖤐\n"
+"└◌─❀*̥˚ ──◌───❀*̥˚┘\n")    
+    
+@borg.on(admin_cmd(pattern=r"g7n"))
+async def catg7n(g7n):
+    await g7n.edit("┌──❀*̥˚──◌──❀*̥˚─┐\n"
+"𝒢𝒩...𝒮𝐿𝐸𝐸𝒫 𝒲𝐸𝐿𝐿....\n"
+"└◌─❀*̥˚ ──◌───❀*̥˚┘\n")   
+    
+@borg.on(admin_cmd(pattern=r"g8m"))
+async def catg8m(g8m):
+    await g8m.edit("•『 𝒢𝑜𝑜𝒹 𝑀𝑜𝓇𝓃𝒾𝓃𝑔♡ 』•\n")
+    
+@borg.on(admin_cmd(pattern=r"g8n"))
+async def catg8n(g8n):
+    await g8n.edit("•『 𝒢𝑜𝑜𝒹 𝒩𝒾𝑔𝒽𝓉...♡ 』•\n")     
+    
+@borg.on(admin_cmd(pattern=r"g9m"))
+async def catg9m(g9m):
+    await g9m.edit("(\__/)\n"
+"(●_●)\n"
+" />🌹 🅶🅼\n"
+"🅷🅰️🆅🅴_🅰️_🅶🆁8_🅳🅰️🆈\n")  
+    
+@borg.on(admin_cmd(pattern=r"g9n"))
+async def catg9n(g9n):
+    await g9n.edit("(\__/)\n"
+"(●_●)\n"
+" />🍷 🅶🅽\n"
+"🆂🅻🅴🅴P_🆆🅴🅻🅻_🆃🅲\n")    
+    
+
