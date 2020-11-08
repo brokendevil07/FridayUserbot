441e9e469c1147862761cfd10c4d07a18250ec4d
MR SPIDY <73451532+MSPIDY@users.noreply.github.com>
2020-11-08 08:46:17 +0530

    Update pmpermit.py

--- fridaybot/modules/pmpermit.py
+++ fridaybot/modules/pmpermit.py
@@ -30,8 +30,8 @@ USER_BOT_WARN_ZERO = "You Have Attempted To Spam Masters Inbox So Inorder To Avo
 
 botisnoob = Var.TG_BOT_USER_NAME_BF_HER
 USER_BOT_NO_WARN = (
-    "**Hello, This is {DEFAULTUSER} Protection Service ⚠️**\n\n"
-    f"`WELCOME TO  {DEFAULTUSER} PM PLEASE BE PATIENTS SIR WILL ALLOW YOU SOON !` \n"
+    "**Hello, This is ✖🅜🅐🅡🅢🅗‿ℳℭℓℓo✖ Protection Service ⚠️**\n\n"
+    f"`WELCOME TO  ✖🅜🅐🅡🅢🅗‿ℳℭℓℓo✖ PM PLEASE BE PATIENTS SIR WILL ALLOW YOU SOON !` \n"
     "**I Request You To Choose A Reason You Have Came For** 👀 \n\n"
     f"**{CUSTOM_MIDDLE_PMP}**")
 
