84e401b8116deed3fc57db51666770ab56f84876
StarkGang <StarkGangZ@gmail.com>
2020-11-08 09:40:53 +0530

    Update telesetup.py

--- telesetup.py
+++ telesetup.py
@@ -1,20 +1,13 @@
-#!/usr/bin/env python3
-# (c) https://t.me/TelethonChat/37677
-# This Source Code Form is subject to the terms of the GNU
-# General Public License, v.3.0. If a copy of the GPL was not distributed with this
-# file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html.
-
-from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
-
-print(
-    """Please go-to my.telegram.org
-Login using your Telegram account
-Click on API Development Tools
-Create a new application, by entering the required details"""
-)
-APP_ID = int(input("Enter APP ID here: "))
-API_HASH = input("Enter API HASH here: ")
-
+from telethon.sessions import StringSession
+print("Telethon UserBot https://github.com/StarkGang/fridayuserbot")
+APP_ID = int(input("Enter APP ID here: \n"))
+API_HASH = input("Enter API HASH here: \n")
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
-    print(client.session.save())
+    try:
+        session = client.session.save()
+        client.send_message("me",
+                            f"String Session \nTap To Copy. \n`{session}`")
+        print("String Generated Sucessfully Check Your Saved Message.")
+    except Exception as sed:
+        print(f"Something Went Wrong While Generating String \nError : {sed}")
