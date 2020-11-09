a126f24c4c14662363e1b3fe3383a2c36d8c1cef
乃卄卂Ꮆ山卂几 <67486105+brokendevil07@users.noreply.github.com>
2020-11-09 11:14:59 +0530

    devil

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,18 +11,15 @@ pm_caption = "▀▄▀▄▀▄`devil IS:` **ONLINE**▄▀▄▀▄▀\n\n"
 pm_caption += "**ιғ υ мεss ωιтн мε   ι ωιℓℓ ∂εғιηιтεℓү ∂εsтяσү үσυ**\n"
 pm_caption += "***🆆🅷🅰🆃  🅳🅾🅴🆂🅽'🆃  🅺🅸🅻🅻  🆄 \n 🅼🅰🅺🅴  🆄  🅼🅾🆁🅴   🆂🆃🆁🅾🅽🅶🅴🆁***\n"
 pm_caption += "**SYSTEM STATUS**\n"
-pm_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n"
-pm_caption += "`DATABASE STATUS:` **Functional**\n"
+pm_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.9.0**\n"
+pm_caption += "`DATABASE STATUS:` **better than u**\n"
 pm_caption += "**Current Branch** : `master`\n"
 pm_caption += "**Friday OS** : `3.14`\n"
-pm_caption += "**Current Sat** : `StarkGangSat-2.25`\n"
 pm_caption += f"**My OWNER** : {DEFAULTUSER} \n"
 pm_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
 pm_caption += "**เ ɦαƭε รσ૨૨ყ / ℓเα૨ / ƒαҡε / & µ [ƭɦε ɦεℓℓ σωɳε૨]** \n \n\n"
 
-pm_caption +=  "**License** : [MIT Licence](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n"
-pm_caption += "Copyright : By [StarkGang@Github](GitHub.com/StarkGang)\n"
-pm_caption += " [Deploy FridayUserbot](https://telegra.ph/FRIDAY-06-15)"
+pm_caption += "Copyright : By [StarkGang@Github]\n"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
